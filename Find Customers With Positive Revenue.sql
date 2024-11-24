a3725c5c46100e6952f68cb0b7d38f9768e464b9
Shady Mahmoud <120749934+Shady129@users.noreply.github.com>
2024-11-24 13:34:40 +0400

    Add files via upload

--- Find Customers With Positive Revenue.sql
+++ Find Customers With Positive Revenue.sql
@@ -1,16 +1,5 @@
 
---Find Customers With Positive Revenue In Year 2021:
-
-
---SQL Schema:
---Create table If Not Exists Customers (customer_id int, year int, revenue int)
-
---insert into Customers (customer_id, year, revenue) values ('1', '2018', '50')
---insert into Customers (customer_id, year, revenue) values ('1', '2021', '30')
---insert into Customers (customer_id, year, revenue) values ('1', '2020', '70')
---insert into Customers (customer_id, year, revenue) values ('2', '2021', '-50')
---insert into Customers (customer_id, year, revenue) values ('3', '2018', '10')
---insert into Customers (customer_id, year, revenue) values ('3', '2016', '50')
+-- Find Customers With Positive Revenue In Year 2021:
 
 
 
